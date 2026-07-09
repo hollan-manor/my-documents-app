c8deebeba8727548f74e1ed4f050841f9d98bfd8
hollan-manor <ivarnomasete@gmail.com>
2026-07-09 03:12:33 +0300

    Simplify service worker to fix loading issue

--- public/sw.js
+++ public/sw.js
@@ -5,17 +5,3 @@ self.addEventListener('install', () => {
 self.addEventListener('activate', () => {
   self.clients.claim()
 })
-
-self.addEventListener('fetch', () => {
-  // Basic passthrough — required for Chrome to consider this installable
-})self.addEventListener('install', () => {
-  self.skipWaiting()
-})
-
-self.addEventListener('activate', () => {
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', () => {
-  // Basic passthrough — required for Chrome to consider this installable
-})
